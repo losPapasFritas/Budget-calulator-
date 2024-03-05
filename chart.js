fe0e10325b4505e7a1330481b5fd5b05053b8de7
losPapasFritas <143848605+losPapasFritas@users.noreply.github.com>
2024-03-05 15:23:18 -0800

    Joshua
    Imported chart.js

---
+++ chart.js
@@ -0,0 +1,35 @@
+import Chart from './node_modules/chart.js/auto'
+
+(async function() {
+
+    const data = {
+        labels: [
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      };
+      
+    new Chart(
+        document.getElementById('chart'),
+        {
+          type: 'doughnut',
+          data: data
+        }
+      );
+
+
+      
+
+})();
+ 
---
+++ dist/index.672d4772.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("monthSalary").addEventListener("change", function(eventData) {
+        let fedTax = 0.12;
+        let stTax = 0.07;
+        let output = 0;
+        let monthvalue = document.getElementById("monthSalary").value;
+        fedTax = monthvalue * fedTax;
+        stTax = monthvalue * stTax;
+        let socSec = monthvalue * 0.062;
+        let medicare = monthvalue * 0.0145;
+        let stDis = monthvalue * 0.01;
+        let retInv = monthvalue * 0.05;
+        output = fedTax + stTax + socSec + medicare + stDis + retInv - 180;
+        console.log(output);
+        document.getElementById("");
+    });
+});
+
+//# sourceMappingURL=index.672d4772.js.map
